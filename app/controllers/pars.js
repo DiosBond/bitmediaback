944a72a6ea5a1208828c5b186817979f7fa8d782
dios <dios@localhost.localdomain>
2020-06-21 23:00:53 +0300

    add check table if exists and insert data fromJSON files

--- app/controllers/pars.js
+++ app/controllers/pars.js
@@ -1,3 +1,14 @@
-let dataFileUser = require('../data/users.json');
-let dataFileStat = require('../data/users_statistic.json');
-console.log(dataFileUser[0]);
+
+//console.log(dataFileUser[0]);
+
+
+// dataFileUser.forEach(elem => {
+// //console.log(elem);
+// // id, first_name, last_name, email, gender, ip_address
+
+// })
+
+// dataFileStat.forEach(elem => {
+//     //console.log(elem);
+//     // user_id, date, page_views, clicks
+// })
--- app/index.js
+++ app/index.js
@@ -1,56 +1,16 @@
-let sqlite3 = require('sqlite3').verbose();
+require("./controllers/dbFunc")
+require("./controllers/pars")
 
-function createDb() {
-    console.log("createDb bitmedia");
-    db = new sqlite3.Database('bitmedia.sqlite3', createTable);
-};
+// const Koa = require('koa');
+// const routers = require('./router/routes');
+// const app = new Koa();
 
-function createTable() {
-    console.log("createTable users");
-    db.run("CREATE TABLE IF NOT EXISTS users (info TEXT)", insertUsers);
-    db.run("CREATE TABLE IF NOT EXISTS users_statistic (info TEXT, info2 TEXT)", insertStatistic);
-}
+// app.use(routers.routes);
+// app.use(routers.allowedMethods);
 
-function insertUsers() {
-    console.log("start insertUsers");
-    var stmt = db.prepare("INSERT INTO users VALUES (?)");
+// app.listen(portNum, function () {
+//     let d = new Date();
+//     console.log('app start at port ', portNum, " " + d.getDate() + "-" + (d.getMonth() + 1) + "-" + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes());
+    
 
-    for (var i = 0; i < 10; i++) {
-        stmt.run("Ipsum " + i);
-    }
-
-    //stmt.finalize(readAllRows);
-}
-
-function insertStatistic() {
-    console.log("start insertStatistic");
-    var stmt = db.prepare("INSERT INTO users_statistic VALUES (?, ?)");
-
-    for (var i = 0; i < 10; i++) {
-        stmt.run("Ipsum " + i, i);
-    }
-
-    //stmt.finalize(readAllRows);
-}
-
-
-let db = new sqlite3.Database('bitmedia.db', (err) => {
-    if (err) {
-
-        console.log(err);
-        if (err.errno == 14) {
-            console.log("True")
-            //createDb();
-        }      //return console.error(err.message);
-      
-    }
-    console.log('Connected to SQlite database.');
-
-    createTable();
-
-    db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='lorem'", function(error, row) {
-		if (row !== undefined) {}})
-
-  });
-
-  require("./controllers/pars")
+// });
